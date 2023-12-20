7671225b16bd90b1134cb62fde03c540340a4ac0
username <90975164+adamswebsite@users.noreply.github.com>
2023-12-20 21:41:25 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sullivansblog.github.io",
+  site: 'https://sullivansblog.github.io',
   integrations: [tailwind(), mdx(), sitemap()],
 });
